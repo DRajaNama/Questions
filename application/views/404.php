3aeb593e50e8e00434eec210548531b5ef38d54b
Dharmendra Nama <rajanamdav@gmail.com>
2019-06-22 10:57:40 +0530

    Update 404.php

--- application/views/404.php
+++ application/views/404.php
@@ -4,7 +4,7 @@
   <title>404 
   </title>
   <meta content="" name="keywords">
-<meta content="" name="description">
+<meta content="back button" name="description">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -20,7 +20,7 @@
     <div class="panel-body">
    <h1 class="text-center text-danger">404 <i class="fa fa-exclamation"></i></h1>
    <h3 class="text-center text-danger">Oops Page Not Found</h3>
-   <center><a class="btn btn-danger" href="<?php echo  $back; ?>">Go To Back</a></center>
+   <center><a class="btn btn-danger" href="<?php echo  $back; ?>">Go To Back </a></center>
     </div>
   </div>
 </div>
